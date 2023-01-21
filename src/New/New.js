da6466825e95a4ce120a23c2920390a932ea0eb2
Motahar Hossain <atuhinbd@gmail.com>
2023-01-21 14:58:08 +0600

    done

---
+++ src/New/New.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const New = () => {
+    return (
+        <div>
+
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            <h1>This is New</h1>
+            
+        </div>
+    );
+};
+
+export default New;
